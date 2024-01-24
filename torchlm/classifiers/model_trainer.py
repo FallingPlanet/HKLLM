efb5c00e509ed24a4b1d1424ce59823ecc407657
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2024-01-24 11:04:43 -0500

    started Universal Classifier

--- torchlm/classifiers/model_trainer.py
+++ torchlm/classifiers/model_trainer.py
@@ -1,1 +1,12 @@
+import torch
+import torch.nn as nn
+from torch.nn import CrossEntropyLoss
+from torch.cuda.amp import GradScaler, autocast
+from sklearn.preprocessing import LabelEncoder
+from torchlm.models import *
+from tqdm import tqdm
+import torchmetrics
 
+class Classifier:
+    def __init__(self, model, device, use_mixed_precision = False, log_dir = None):
+    

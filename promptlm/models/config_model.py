e5ce76ffdcb06836ff61b5e9c9d260ccd6ff3a1e
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2024-01-25 08:26:43 -0500

    Update config_model.py
    Models have been added, quantization configurations next

--- promptlm/models/config_model.py
+++ promptlm/models/config_model.py
@@ -22,18 +22,27 @@ class get_model:
             tokenizer = AutoTokenizer.from_pretrained(model_id)
             return base_model, tokenizer
         def get_gpt2_XL():
-            model_id = ""
+            model_id = "gp2-xl"
+            base_model = AutoModelForCausalLM.from_pretrained(model_id)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
+            return base_model, tokenizer
         def get_gpt2_large():
-            model_id = ""
+            model_id = "gpt2-large"
+            base_model = AutoModelForCausalLM.from_pretrained(model_id)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
+            return base_model,tokenizer
         def get_gpt2_medium():
-            model_id = ""
+            model_id = "gpt2-medium"
+            base_model = AutoModelForCausalLM.from_pretrained(model_id)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
+            return base_model, tokenizer
         def get_gpt2():
             model_id = "gpt2"
             base_model = AutoModelForCausalLM.from_pretrained(model_id)
             tokenizer = AutoTokenizer.from_pretrained(model_id)
             return base_model, tokenizer
         def get_gpt():
-            model_id = ""
+            model_id = "openai-gpt"
             base_model = AutoModelForCausalLM.from_pretrained(model_id)
             tokenizer = AutoTokenizer.from_pretrained(model_id)
             return base_model, tokenizer
@@ -43,38 +52,56 @@ class get_model:
             tokenizer = LlamaTokenizerFast.from_pretrained(model_id)
         def get_llama2_7b(base=False,auth_token=""):
             if base == True:
-                model_id = ""
+                model_id = "meta-llama/Llama-2-7b-hf"
             else:
-                model_id = ""
+                model_id = "meta-llama/Llama-2-7b-chat-hf"
             base_model = AutoModelForCausalLM.from_pretrained(model_id)
             tokenizer = AutoTokenizer.from_pretrained(model_id)
         def get_llama2_13b(base=False,auth_token=""):
             if base == True:
-                model_id = ""
+                model_id = "meta-llama/Llama-2-13b-hf"
             else:
-                model_id = ""
+                model_id = "meta-llama/Llama-2-13b-chat-hf"
             base_model = LlamaForCausalLM.from_pretrained(model_id)
             tokenizer = LlamaTokenizerFast.from_pretrained(model_id)
         def get_llama2_70b(base=False,auth_token=""):
             if base == True:
-                model_id = ""
+                model_id = "meta-llama/Llama-2-70b-hf"
             else:
-                model_id = ""
+                model_id = "meta-llama/Llama-2-70b-chat-hf"
             base_model = AutoModelForCausalLM.from_pretrained(model_id)
             tokenizer = AutoTokenizer.from_pretrained(model_id)
         def get_bert_tiny():
+            model_id = "prajjwal1/bert-tiny"
+            base_model = AutoModelForCausalLM.from_pretrained(model_id)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
             pass
         def get_distilbert():
+            model_id = "distilbert-base-uncased"
+            base_model = AutoModelForCausalLM.from_pretrained(model_id)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
             pass
         def get_roberta():
+            model_id = "roberta-uncased"
+            base_model = AutoModelForCausalLM.from_pretrained(model_id)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
             pass
         def get_bert_base():
+            model_id = "bert-base-uncased"
+            base_model = AutoModelForCausalLM.from_pretrained(model_id)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
             pass
         def get_bert_small():
+            model_id = "prajjwal1/bert-small"
+            base_model = AutoModelForCausalLM.from_pretrained(model_id)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
             pass
         def get_bert_large():
             pass
         def get_bert_medium():
+            model_id = "prajjwal1/bert-medium"
+            base_model = AutoModelForCausalLM.from_pretrained(model_id)
+            tokenizer = AutoTokenizer.from_pretrained(model_id)
             pass
         
 

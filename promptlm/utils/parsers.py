2d5f945ad6e43dc0e74a2480635d3f644e33276c
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2024-01-26 18:55:26 -0500

    Update parsers.py with multc_parser
    The function takes an array of strings as input, these are designed to be the target classes that we are looking to be present in the prompt. (if each index can be decoded into a binary), It checks if the target is contained in the model output, and depending on output_type, returns a Boolean array or Binary array, with True,1 representing a hit, and a False, 0 representing a miss and there it was not found in the text

--- promptlm/utils/parsers.py
+++ promptlm/utils/parsers.py
@@ -1,3 +1,23 @@
-def output_parser(input = []):
+def multc_parser(target, text, output_type = "bool"):
+    if not isinstance(target, list):
+        raise TypeError("target must be a list of classes")
+    if not isinstance(text, str):
+        raise TypeError("model output must be a String.")
+    for x in target:
+        if not isinstance(x, str):
+            raise TypeError("All elements in the target must be a string")
+        
+    b_array = []
+    if output_type == "bool":
+        for element in target:
+            if element in text: b_array.append(True)
+            else: b_array.append(False)
+        return b_array
+    if output_type == "int":
+        for element in target:
+            if element in text: b_array.append(1)
+            else: b_array.append(0)
+        return b_array
+    
     
     

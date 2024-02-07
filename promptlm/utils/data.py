912849cce645f2dd037a1fb930333ea5865ad365
WilliamAStigall <williamastigall@gmail.com>
2024-02-07 11:21:47 -0500

    Further changes?

--- promptlm/utils/data.py
+++ promptlm/utils/data.py
@@ -12,22 +12,35 @@ def prepare_dataset_for_inference(df, text_col, class_col, sample_size):
 
 def generate_shot_examples(data_dict,   shot_examples):
     
-    
     df = pd.DataFrame(data_dict)
+    
+    shot_examples_dict = {'x': [], 'y':[]}
+    
+    indicies_to_remove = []
+    
     unique_classes = df['y'].unique()
     
-    shot_examples_dict = {'x': [], 'y':[]
-    }
     for cls in unique_classes:
-        class_samples = df[df['y']] == cls
+        class_samples = df[df['y'] == cls]
         
-        sampled = class_samples.sample(min(len(class_samples),shot_examples))
+        n_samples = min(len(class_samples),shot_examples)
+        sampled = class_samples.sample(n_samples)
         
         shot_examples_dict['x'].extend(sampled['x'].tolist())
         shot_examples_dict['y'].extend(sampled['y'].tolist())
         
-        return shot_examples_dict
-    pass
+        indicies_to_remove.extend(sampled.index.tolist())
+    
+    df_remaining = df.drop[indicies_to_remove]
+    
+    remaining_data_dict = df.remaining.to_dict['list']
+    
+    return shot_examples_dict,remaining_data_dict
+        
+        
+    
+    
+    
 
 def prepare_dataset_for_training():
     pass

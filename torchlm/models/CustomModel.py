1442dd311422490e5c14bc81a38ee371ae80f96c
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2024-02-14 14:37:03 -0500

    Create CustomModel.py

---
+++ torchlm/models/CustomModel.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Custom_Model(nn.Module):
+    def __init__(self, **kwargs):
+        super(Custom_Model, self).__init__():
+        from_checkpoint = kwargs.get('model_dict',None)
+        
+            
